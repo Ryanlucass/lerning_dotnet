a90a4f15b26aab7431a6618a8201f0726bc5f883
Lucas alves <lucasryanalves@gmail.com>
2020-12-12 15:04:01 -0300

    update

---
+++ aula03/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace aula03
+{
+    class Aula03
+    {
+        static void Main()
+        {
+        // tipo/nome/valor
+        // variável inteiro: 
+         int num = 0; 
+         //variável char, só pode armazenar um caractere 
+         char letra = 'L';
+          //variável float
+         float valor = 3.5f;
+         //variável string 
+         string nome = "Lucas Alves";
+         // quando eu uso o var, eu não específico o tipo, ele indentifica na hora de compilar
+         var aux= nome;
+
+         Console.WriteLine(aux);
+           
+        }
+    }
+}
+
+
+// using System;
+
+// namespace Aula03{
+//     // criando a classe
+//     public class Aula03{
+//     // cruando a função da classe:
+//         static void variaveis(){
+//         // tipo/nome/valor
+//         // variável inteiro: 
+//         int num = 0; 
+//         //variável char, só pode armazenar um caractere 
+//         char letra = 'L';
+//          //variável float
+//         float valor = 3.5f;
+//         //variável string 
+//         string nome = "Lucas Alves";
+//         // quando eu uso o var, eu não específico o tipo, ele indentifica na hora de compilar
+//         var aux= nome;
+
+//         Console.WriteLine(aux);
+//         }
+//     }
+// }
