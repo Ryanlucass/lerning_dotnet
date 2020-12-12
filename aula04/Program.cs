1190527aa17edd888bfd7ac73fea15d40eafce78
Lucas alves <lucasryanalves@gmail.com>
2020-12-12 18:11:21 -0300

    update

---
+++ aula04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+// criação de métodos 
+namespace aula04
+{
+    class Aula04
+    {
+        static void Main()
+        {
+           int num=0;
+        }
+        void teste(){
+            Console.WriteLine(num);
+        }
+
+    }
+}
---
+++ aula04/README.md
@@ -0,0 +1,1 @@
+# Aplicando métodos 
