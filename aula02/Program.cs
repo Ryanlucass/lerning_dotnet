040d488b416fd5089163b7db14c437377952f627
Lucas alves <lucasryanalves@gmail.com>
2020-12-12 12:43:39 -0300

    update

---
+++ aula02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+// usando a biblioteca padrão de entrada e saída
+
+namespace aula02 
+// local onde vão ser organizados
+{
+    class Program
+    // classe princpipal do nosso arquivo, já que estamos utilizando POO
+    {
+        // metodo principal da nossa classe 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testando os argumentos");
+            if(args.GetLength(0)>0)
+            {
+            Console.Write(args.GetValue(0));
+            }
+        }
+    }
+}
+
+// se eu colocar um argumento de entrada na hora de utilziar o programa, ele vai pegar a posição zero, e enviar para lá 
