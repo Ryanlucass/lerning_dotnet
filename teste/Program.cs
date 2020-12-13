34da4cf2581d2f54a4e4a67876ed36a4ec641581
Lucas alves <lucasryanalves@gmail.com>
2020-12-12 22:04:39 -0300

    update

--- teste/Program.cs
+++ teste/Program.cs
@@ -4,12 +4,35 @@ namespace lerning_dotnet
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            oi oi = new oi();
-            Console.WriteLine("Hello World!");
+        int DataNascimento;
+        string Nome;
+        long CPF,CEP;
 
-            oi.BoaNoite();
+        
+        
+        Console.WriteLine("\nPor favor, preenche o formulário abaixo para cadastrar suas informações no sistema.\n");
+        
+        Console.WriteLine("\nDigite o seu nome: ");
+        Nome=Console.ReadLine();
+
+        Console.WriteLine("\nDigite aqui a sua data de nascimento(sem barras): ");
+        DataNascimento=int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nDigite o seu CPF (somente números): ");
+        CPF=long.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nDigite o seu CEP: ");
+        CEP=long.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nO seu formulário está completo, confirmando os dados inseridos: ");
+
+        Console.WriteLine("Nome....................:"+Nome);
+        Console.WriteLine("Data de Nascimento......:"+DataNascimento);
+        Console.WriteLine("CPF.....................:"+CPF);
+        Console.WriteLine("CEP.....................:"+CEP);
+
         }
     }
 }
