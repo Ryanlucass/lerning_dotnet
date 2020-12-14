e7b13c94cb87ad6a7c149da8778084ef297e62aa
Lucas alves <lucasryanalves@gmail.com>
2020-12-14 19:07:55 -0300

    update

---
+++ aula12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace aula12
+{
+    class condicionais
+    {
+        static void Main()
+        {
+          Console.WriteLine("Digite a nota do primeiro bimestre");
+          nota=float.Parse(Console.ReadLine);
+
+          if(nota>6.0){
+              Console.WriteLine("Parabéns, você foi aprovado!");
+          }
+          else{
+              Console.WriteLine("você foi reprovado");
+          }
+
+        }
+    }
+}
