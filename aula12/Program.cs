2d7b6cf1c49e6f73c61d94f6d168f85e939dfb83
Lucas alves <lucasryanalves@gmail.com>
2020-12-14 19:33:56 -0300

    update

--- aula12/Program.cs
+++ aula12/Program.cs
@@ -15,6 +15,9 @@ namespace aula12
           if(nota >= 6.0){
               Console.WriteLine("Parabéns, você foi aprovado!");
           }
+          else if(nota < 4){
+              Console.WriteLine("Você precisará fazer a av3");
+          }
           else{
               Console.WriteLine("você foi reprovado");
           }
