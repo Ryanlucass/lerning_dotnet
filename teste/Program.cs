aed7826842c44d20749739dc746aea3c4150f599
Lucas alves <lucasryanalves@gmail.com>
2020-12-13 23:19:29 -0300

    update

--- teste/Program.cs
+++ teste/Program.cs
@@ -9,12 +9,13 @@ namespace lerning_dotnet
         string DataNascimento,Nome,CPF,CEP;
 
         
-        Console.WriteLine("\nPor favor, preenche o formulário abaixo para cadastrar suas informações no sistema.\n");
+        Console.WriteLine("\nPor favor, preencha o formulário abaixo para cadastrar suas informações no sistema.\n");
+        Console.WriteLine("\nColocar todas as pontuações necessárias\n");
         
         Console.WriteLine("\nDigite o seu nome: ");
         Nome=Console.ReadLine();
 
-        Console.WriteLine("\nDigite aqui a sua data de nascimento(sem barras): ");
+        Console.WriteLine("\nDigite aqui a sua data de nascimento: ");
         DataNascimento=Console.ReadLine();
 
         Console.WriteLine("\nDigite o seu CPF : ");
@@ -25,10 +26,10 @@ namespace lerning_dotnet
 
         Console.WriteLine("\nO seu formulário está completo, confirmando os dados inseridos: ");
 
-        Console.WriteLine("Nome....................:"+Nome);
-        Console.WriteLine("Data de Nascimento......:"+DataNascimento);
-        Console.WriteLine("CPF.....................:"+CPF);
-        Console.WriteLine("CEP.....................:"+CEP);
+        Console.WriteLine("Nome....................: "+Nome);
+        Console.WriteLine("Data de Nascimento......: "+DataNascimento);
+        Console.WriteLine("CPF.....................: "+CPF);
+        Console.WriteLine("CEP.....................: "+CEP);
 
         }
     }
