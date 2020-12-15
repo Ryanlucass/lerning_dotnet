d6399d2f26250b3fd306239386c8571aa98d9acc
Lucas alves <lucasryanalves@gmail.com>
2020-12-15 09:59:11 -0300

    up

--- aula19/aula19.cs
+++ aula19/aula19.cs
@@ -8,8 +8,8 @@ namespace aula19
         {
             int[] n={33,44,55,66,44};
  
-
-           for(int s=0; s<5 ; s++){
+            //ele conta o tamanho do meu array
+           for(int s=0; s<n.Length; s++){
             //Console.WriteLine(n[s]);
 
             Console.WriteLine("O valor do número na posição {0}: {1}",s,n[s]);
