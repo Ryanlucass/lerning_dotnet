46698d3603d5b2771474f82f1fdf65577b10527d
Lucas alves <lucasryanalves@gmail.com>
2020-12-15 09:32:57 -0300

    update

---
+++ aula18/aula18.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace aula18
+{
+    class bidimensionais
+    {
+        static void Main()
+        {
+            int[,] num=new int[2,2]{{1,2},{3,4}};
+
+            Console.Write(num[0,1]);
+
+        }
+    }
+}
---
+++ aula19/aula19.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace aula19
+{
+    class repetição
+    {
+        static void Main()
+        {
+           for(int num=0; num<10 ; num++){
+               
+               Console.WriteLine("valor de num:{0}",num);
+
+           }
+        }
+    }
+}
