e4bb8bd2050444681e5939d80d523376289e9d6e
Lucas alves <lucasryanalves@gmail.com>
2020-12-18 10:01:04 -0300

    update

---
+++ aula20/aula20.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula20
+{
+    class repeticao
+    {
+        static void Main()
+        {
+            Console.WriteLine("Trabalhando com estruturas de repetição while");
+
+            int[] num=new int[10];
+
+            int i=0;
+            while(i<num.Length){
+            //vai executar até que essa condição não seja mais verdadeira 
+                num[i]=0;
+
+                Console.WriteLine(num[i]);
+                i++;
+            }
+
+        }
+    }
+}
