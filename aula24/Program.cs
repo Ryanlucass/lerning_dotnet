048903872fda5ee96f9864ca61c514e2461261bb
Lucas alves <lucasryanalves@gmail.com>
2020-12-20 11:13:53 -0300

    metodos

---
+++ aula24/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aula24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
