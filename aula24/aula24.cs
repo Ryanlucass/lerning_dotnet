a7391e4a0324d58829c928e5575d4d89ca7a8b93
Lucas alves <lucasryanalves@gmail.com>
2020-12-20 11:29:38 -0300

    update

--- aula24/aula24.cs
+++ aula24/aula24.cs
@@ -7,11 +7,8 @@ namespace aula24
         static void Main()
         {
             //chamando o método na função principal
-            for(int i=0; i<3 ; i++){
-
-            boasvindas();
-
-            }
+            //quando chamar um método que possui argumento é necessário colocar o seu valor 
+            somar(2342340,5234234);
         }
 
         static void boasvindas()
@@ -21,6 +18,12 @@ namespace aula24
 
         Console.WriteLine("Seja bem-vindo");
 
+        }
+        //inicializando o método com parâmetros de entrada 
+        static void somar(int n1, int n2)
+        {
+            int res=n1+n2;
+            Console.WriteLine("A soma de {0} e {1} é: {2}",n1,n2,res);
         }
 
     }
