99c064397dfb2cfd34f2e75508d5a7ff9daaa184
Lucas alves <lucasryanalves@gmail.com>
2020-12-20 10:54:09 -0300

    foreach update

---
+++ aula22/aula22.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace aula22
+{
+    class repeticao
+    {
+        static void Main()
+        {
+            int[] num=new int [5]{11,22,33,44,55};
+            //a estrutura do foreach 
+
+            //ele serve para criar um contador, não tem a quantidade de opção igual ao for
+            foreach(int n in num){
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
