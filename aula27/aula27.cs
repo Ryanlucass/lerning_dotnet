1483141e3ef8c4b40d0d87cfd803b7e39efcfd44
Lucas alves <lucasryanalves@gmail.com>
2020-12-25 22:50:29 -0300

    update

--- aula27/aula27.cs
+++ aula27/aula27.cs
@@ -6,7 +6,14 @@ namespace aula27
     {
         static void Main()
         {
-            
+         Console.WriteLine("Função Main");
+        }
+
+//trabalhando com os argumentos params 
+        static void soma(int n1, int n2)
+        {
+            int res= n1+n2;
+            Console.WriteLine("A soma de {0} com {1} é {2}",n1,n2,res);
         }
     }
 }
