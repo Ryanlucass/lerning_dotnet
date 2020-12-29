cdc399dedad7405d2ba02fa1b9c48b7e8548d9d8
Lucas alves <lucasryanalves@gmail.com>
2020-12-29 14:51:06 -0300

    update

--- aula34/Program.cs
+++ aula34/Program.cs
@@ -4,7 +4,7 @@ namespace aula34 //trabalhando com herança
 {
     class Veiculo //classe base
     {
-
+        public int rodas;
         public int velMax;
         private bool ligado;
         public void ligar(){
@@ -14,23 +14,46 @@ namespace aula34 //trabalhando com herança
             ligado=false;
         }
 
+        public string getLigado()
+        {
+            if(ligado){
+                return "sim";
+            }else
+            {
+                return "não";
+            }
+            
+        }
+
     }
 
     class Carro:Veiculo{
+        public string nome;
+        public string cor;
+
     
-    public Carro()
-    {
-        
-    }
+        public Carro(String nome,string cor)
+        {
+        //Não consigo usar o objeto ligado, já que ele é private. mesmo sendo herdado.
+            desligar();
+            rodas=4;
+            velMax=120;
+            this.nome=nome;
+            this.cor=cor;
+        }
 
     }
     class Program
     {
         static void Main()
         {
-         Carro c1=new Carro();
+         Carro c1=new Carro("Chevete","Vermelho");
 
-         c1.ligado=true;
+         Console.WriteLine("Cor:.......{0}",c1.cor);
+         Console.WriteLine("Nome:......{0}",c1.nome);
+         Console.WriteLine("Rodas:.....{0}",c1.rodas);
+         Console.WriteLine("Vel.Maxima:{0}",c1.velMax);
+         Console.WriteLine("Ligado.....{0}",c1.getLigado());  
         }
     }
 
