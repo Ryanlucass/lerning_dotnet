37b3a9841b1b0f6d98be9fc653d13d220fca21ad
Lucas alves <lucasryanalves@gmail.com>
2020-12-29 14:34:50 -0300

    update

---
+++ aula34/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace aula34 //trabalhando com herança
+{
+    class Veiculo //classe base
+    {
+
+        public int velMax;
+        private bool ligado;
+        public void ligar(){
+            ligado=true;
+        }
+        public void desligar(){
+            ligado=false;
+        }
+
+    }
+
+    class Carro:Veiculo{
+    
+    public Carro()
+    {
+        
+    }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+         Carro c1=new Carro();
+
+         c1.ligado=true;
+        }
+    }
+
+
+
+
+
+}
