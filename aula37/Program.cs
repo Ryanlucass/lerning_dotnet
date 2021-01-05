19418075ed170b59a914f44d096215efcd361202
Lucas alves <lucasryanalves@gmail.com>
2021-01-05 08:39:06 -0300

    update

--- aula37/Program.cs
+++ aula37/Program.cs
@@ -2,11 +2,40 @@
 
 namespace aula37
 {
+    
+    class Base
+    {
+        public Base(){
+            Console.WriteLine("Construtor da classe Base");
+        }
+
+    }
+
+    class Derivada1:Base{
+
+        public Derivada1(){
+            Console.WriteLine("Construtor da classe Derivada 1");
+        }
+
+    }
+    
+    class Derivada2:Derivada1{
+
+        public Derivada2(){
+            Console.WriteLine("Construtor da classe Derivada 2");
+        }
+
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Derivada2 derivada2=new Derivada2();
+           
         }
     }
+
+
+
 }
