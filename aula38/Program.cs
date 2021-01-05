f2be119fe260b0fa6a51a30f22e67e37305440cd
Lucas alves <lucasryanalves@gmail.com>
2021-01-05 08:41:44 -0300

    update

---
+++ aula38/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace aula38
+{
+
+    //trabalhando com métodos virtuais 
+    
+    
+    
+    
+    
+    
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine();
+        }
+    }
+
+
+
+
+
+}
