72ba43570e849f569fb6b09013260eda9d86d1ec
Lucas alves <lucasryanalves@gmail.com>
2021-01-05 09:31:23 -0300

    update

--- aula38/Program.cs
+++ aula38/Program.cs
@@ -2,23 +2,56 @@
 
 namespace aula38
 {
-
-    //trabalhando com métodos virtuais 
-    
-    
-    
-    
-    
-    
-    class Program
+//Trabalhando com métodos virtuais 
+//são métodos que possuem os mesmos nomes, porém em classes diferentes 
+    class Base
     {
-        static void Main()
-        {
-            Console.WriteLine();
+        public Base(){
+            Console.WriteLine("Construtor da classe Base");
         }
+
+        virtual public void info(){
+            Console.WriteLine("Base");
+        }
+
     }
 
+    class Derivada1:Base{
 
+        public Derivada1(){
+            Console.WriteLine("Construtor da classe Derivada 1");
+        }
+        override public void info(){
+            //quando houver override, ele existe na classe base e estás endo sobreescrito 
+            Console.WriteLine("Derivada 01");
+        }
+
+    }
+    
+    class Derivada2:Derivada1{
+
+        public Derivada2(){
+            Console.WriteLine("Construtor da classe Derivada 2");
+        }
+
+        override public void info(){
+            //quando houver override, ele existe na classe base e estás endo sobreescrito 
+            Console.WriteLine("Derivada 02");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Base Ref;
+        Derivada1 derivada1=new Derivada1();
+        Derivada2 derivada2=new Derivada2();
+           
+        Ref=derivada1;
+        Ref.info();
+           
+        }
+    }
 
 
 
