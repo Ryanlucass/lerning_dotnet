96d7a7409c56fa8e59d73b96ae6120e7d0516811
Lucas alves <lucasryanalves@gmail.com>
2021-01-06 19:34:00 -0300

    update

--- aula39/Program.cs
+++ aula39/Program.cs
@@ -1,7 +1,29 @@
 ﻿using System;
-
+//trabalhando com classes abstratas 
 namespace aula39
 {
+    
+    abstract class Veiculo
+    {
+        protected bool ligado;
+        protected int veLmax;
+        protected int velAtual;
+
+      public Veiculo()
+      {
+        //normalmente usa o construtor para colocar os valores nos atributos 
+        ligado=true;
+        velAtual=0;
+        veLmax=100;
+      }
+
+
+
+
+      
+    }
+    
+    
     class Program
     {
         static void Main()
@@ -9,4 +31,8 @@ namespace aula39
             
         }
     }
+
+
+
+
 }
