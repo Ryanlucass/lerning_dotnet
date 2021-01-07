b82b36282d257a13a02d7f4e03a431b7a8ac8b09
Lucas alves <lucasryanalves@gmail.com>
2021-01-07 19:28:56 -0300

    update

---
+++ rcn/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace rcn
+{
+//Crie 3 classes: Carro, Moto, Bicicleta
+//defina (onde se aplique) os métodos: LigarMotor(), Emplacar(), Acelerar() e Frear().
+//Crie uma lista com 10 veículos considerando todas as classes criadas anteriormente, percorra cada Veículo e execute //os métodos aplicaveis a cada um.
+
+
+    abstract class Veiculo{
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
