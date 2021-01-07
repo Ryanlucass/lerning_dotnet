48621d666a68b9dfb44727945d1570ed6d101855
Lucas alves <lucasryanalves@gmail.com>
2021-01-07 00:30:29 -0300

    update

--- aula39/Program.cs
+++ aula39/Program.cs
@@ -14,30 +14,27 @@ namespace aula39
         //normalmente usa o construtor para colocar os valores nos atributos 
         ligado=true;
         velAtual=0;
-        veLmax=100;
       }
-      public void setLigado(bool conectado)
+      public void setLigado(bool ligado)
       {
-        conectado=ligado;
+        this.ligado=ligado;
       }
 
-      abstract public void setVelAtual(int velocidade)
-      {
-
+      public int getVelAtual(){
+          return velAtual;
       }
 
-
-
-
+      abstract public void aceleration(int velocidade);
+      //ele não pode sequer ter as chavess
     }
     class Carro:Veiculo
     {
        public Carro(){
-           veLmax:120;
-
+           veLmax=120;
        }
-        override public void setVelAtual(int velocidade){
-            velAtual+=10*velocidade; 
+        override public void aceleration(int velocidade){
+            //estou reescrevendo a função 
+            velAtual+=10*velocidade;
       }
 
 
@@ -49,7 +46,11 @@ namespace aula39
     {
         static void Main()
         {
-            
+            Carro car01=new Carro();
+
+            car01.aceleration(13);
+            // car01.aceleration(-1);
+            Console.WriteLine("O valor atual da velocidade é {0}",car01.getVelAtual());
         }
     }
 
