a72a0807445bdadb01c6e0ac245a29ddfe0a25fa
Lucas alves <lucasryanalves@gmail.com>
2021-01-11 10:45:12 -0300

    update aula41
    Get e Set

---
+++ aula41/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace aula41
+{
+    class Carro{
+        //trabalhando com Acessors GET e SET
+
+        private int Velmax;
+
+        public int vm{
+            get{
+                return Velmax;
+            
+            }set{
+                if(value < 0){
+                    Velmax=0;
+                }
+                else if (value > 300){
+                    Velmax=300;
+                }else{
+                    Velmax=value;
+                }
+            }
+        }
+       
+       
+        public Carro(){
+            vm=120;
+        }
+
+
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Carro c1=new Carro();
+
+            c1.vm=310;
+            Console.WriteLine("Velocidade...{0}",c1.vm);
+        }
+    }
+
+}
