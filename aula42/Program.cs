8bdf6e615db0d33bc108e4aafdf236383635fc4e
Lucas alves <lucasryanalves@gmail.com>
2021-01-12 09:34:55 -0300

    Update aula 42
    trabalhando com indexadores de classes

---
+++ aula42/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace aula42
+{
+    class Carro{
+        //trabalhando com Acessors GET e SET
+
+        private int[] Velmax=new int[5]{80,120,160,240,300};
+
+        public int this[int i]{
+            get{
+                return Velmax[i];
+            
+            }set{
+                if(value < 0){//value: valor passado
+                    Velmax[i]=0;
+                }else if (value > 300){
+                    Velmax[i]=300;
+                }else{
+                    Velmax[i]=value;
+                }
+            }
+        }
+       
+
+        public Carro(){//construtor 
+        
+        }
+
+
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Carro c1=new Carro();
+
+            c1[4]=455;
+            Console.WriteLine("Velocidade...{0}",c1.vm);
+        }
+    }
+
+}
