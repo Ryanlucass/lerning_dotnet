cbb92d586a29e6293ab5a7a78c89b6b6168f32d7
Lucas alves <lucasryanalves@gmail.com>
2021-01-12 19:32:26 -0300

    update

--- aula43/Program.cs
+++ aula43/Program.cs
@@ -4,27 +4,64 @@ namespace aula43
 {
 
     public interface Veiculo{
-        void ligar();
-        void desligar();
+        string ligar();
+        string desligar();
         void info();
     }
 
     public interface Combate{
-        void disparar();
+        string disparar();
     }
     
     
-        class Carro{
-            public Carro(){
-                
-            }
+        class Carro:Veiculo,Combate{
 
+        public bool ligado;
+        public int ammo;
+        public Carro(){
+            ammo=0;
+            ligado=false;
+        }
+
+        public string ligar(){
+            if(ligado == true){
+            return "O carro está ligado";
+            }else{
+                return "deu erro";
+            }
+            
+        }
+        public string desligar(){
+           if(ligado == false){
+            return "O carro está desligado";
+           }else{
+               return"deu erro";
+           }
+
+        }
+        public string disparar(){
+            if(ammo >0){
+            return "O carro tem munição";
+            }else{
+            return"O carro não tem munição suficiente";
+            }  
+        
+        }
+        public void info(){
+            Console.WriteLine("A situação do carro é: {0}, {1}",ligar(),disparar());
+        }
     }
+
+
+
+
         class Program
     {
         static void Main()
         {
-            
+            Carro c1=new Carro();
+
+            c1.info();
         }
     }
 }
