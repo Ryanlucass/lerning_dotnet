19b800406eec505e2264e5ce369a6f9d73ae9d34
Lucas alves <lucasryanalves@gmail.com>
2021-01-12 19:09:58 -0300

    update interface
    Trabalhando com interfaces

---
+++ aula43/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace aula43
+{
+
+    public interface Veiculo{
+        void ligar();
+        void desligar();
+        void info();
+    }
+
+    public interface Combate{
+        void disparar();
+    }
+    
+    
+        class Carro{
+            public Carro(){
+                
+            }
+
+    }
+        class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
