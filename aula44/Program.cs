7322048e88a19ef947b5d48d86f3ef948c75ecf7
Lucas alves <lucasryanalves@gmail.com>
2021-01-19 20:51:20 -0300

    update
    Trabalhando com operador struct

--- aula44/Program.cs
+++ aula44/Program.cs
@@ -3,19 +3,35 @@
 namespace aula44
 {
 
-    
-    
-    
-    
-    
+    struct Robo{
+
+        public string nome;
+        public string armas;
+
+        public string sistema_operacional;
+
+        public Robo(string nome, string armas, string sistema_operacional){
+            this.armas=armas;
+            this.sistema_operacional=sistema_operacional;
+            this.nome=nome;
+        }
+
+    }    
+        
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
-            
-            
-            
+    
+            Robo r1;
+
+            r1.nome  = "Treme-Treme";
+            r1.armas = "Sabre de luz";
+            r1.sistema_operacional = "IOS";
+
+            Console.WriteLine("Nome........{0}",r1.nome);
+            Console.WriteLine("Armas.......{0}",r1.armas);
+            Console.WriteLine("S.O.........{0}",r1.sistema_operacional);
         }
     }
 
