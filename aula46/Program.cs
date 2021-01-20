1b636c5e0b1495d6c6e1336f710ca5a76c0988d5
Lucas alves <lucasryanalves@gmail.com>
2021-01-20 10:03:17 -0300

    update

--- aula46/Program.cs
+++ aula46/Program.cs
@@ -2,11 +2,47 @@
 
 namespace aula46
 {
+
+    class Galinha{
+
+        private string NomeGalinha;
+        private int QuantidadeOvo;
+
+
+        public Galinha(string NomeGalinha, int QuantidadeOvo){//definindo os valores para os argumentos e objetos 
+            this.NomeGalinha=NomeGalinha;
+            QuantidadeOvo=0;
+        }
+
+        //criando o método que vai retornar o objeto
+        public Ovo botar(){// ele retorna um objeto ovo  
+            
+            return new Ovo();
+        }
+
+
+
+    }
+
+    class Ovo{
+
+        public Ovo(){//construtor 
+        //toda vez que eu chamar ovo, ele vai dizer que está criado
+            Console.WriteLine("Ovo criado");
+        }
+
+
+    }
+    
+    
+    
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
+
+
 }
