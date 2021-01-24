743a18f71d101b6848e03649a1488db7290bbf3d
Lucas alves <lucasryanalves@gmail.com>
2021-01-24 09:58:35 -0300

    update

--- aula48/Program.cs
+++ aula48/Program.cs
@@ -8,11 +8,14 @@ namespace aula48
 
     class Fatorial{
 
-    int Fatorial(int n){//construtor retornar "int"
+    public int fatorial(int n){//construtor retornar "int"
         int res;
-        if(n<=1){
+        if(n<=1){//caso o argumento de entrada for menor que 1, não da pra fazer fatorial.
             res=1;
+        }else{
+            res=n*fatorial(n-1);
         }
+        return res;
     }
 
 
@@ -20,9 +23,17 @@ namespace aula48
 
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Fatorial fac=new Fatorial();
+
             
+        
+            Console.WriteLine("Digite o número que você quer saber o fatorial: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            int res = fac.fatorial(n);
+            Console.WriteLine(res);
         }
     }
 }
