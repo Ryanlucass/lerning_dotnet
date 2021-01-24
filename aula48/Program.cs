588bffd66ae1a5078ec549be43e8bb582c251586
Lucas alves <lucasryanalves@gmail.com>
2021-01-24 09:30:20 -0300

    update
    Recursividade

---
+++ aula48/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace aula48
+{
+
+    //trabalhando com Recusividade
+
+
+    class Fatorial{
+
+    int Fatorial(int n){//construtor retornar "int"
+        int res;
+        if(n<=1){
+            res=1;
+        }
+    }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
