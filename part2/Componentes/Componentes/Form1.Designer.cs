1c65c10b8540fa8e0bef677e966e626d7b25ad5c
Lucas alves <lucasryanalves@gmail.com>
2021-01-26 08:46:47 -0300

    update

--- part2/Componentes/Componentes/Form1.Designer.cs
+++ part2/Componentes/Componentes/Form1.Designer.cs
@@ -31,13 +31,14 @@ namespace Componentes
         {
             this.btn_adicionar = new System.Windows.Forms.Button();
             this.tb_veiculo = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btn_limpar = new System.Windows.Forms.Label();
             this.tb_lista_veiculos = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_adicionar
             // 
-            this.btn_adicionar.Location = new System.Drawing.Point(188, 21);
+            this.btn_adicionar.Location = new System.Drawing.Point(188, 26);
             this.btn_adicionar.Name = "btn_adicionar";
             this.btn_adicionar.Size = new System.Drawing.Size(92, 31);
             this.btn_adicionar.TabIndex = 0;
@@ -51,14 +52,14 @@ namespace Componentes
             this.tb_veiculo.Size = new System.Drawing.Size(170, 22);
             this.tb_veiculo.TabIndex = 1;
             // 
-            // label1
+            // btn_limpar
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 1);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Digite um Veículo";
+            this.btn_limpar.AutoSize = true;
+            this.btn_limpar.Location = new System.Drawing.Point(12, 9);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(117, 17);
+            this.btn_limpar.TabIndex = 2;
+            this.btn_limpar.Text = "Digite um Veículo";
             // 
             // tb_lista_veiculos
             // 
@@ -68,13 +69,23 @@ namespace Componentes
             this.tb_lista_veiculos.Size = new System.Drawing.Size(268, 264);
             this.tb_lista_veiculos.TabIndex = 3;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // F_Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(300, 362);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.tb_lista_veiculos);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_limpar);
             this.Controls.Add(this.tb_veiculo);
             this.Controls.Add(this.btn_adicionar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -91,8 +102,9 @@ namespace Componentes
 
         private System.Windows.Forms.Button btn_adicionar;
         private System.Windows.Forms.TextBox tb_veiculo;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label btn_limpar;
         private System.Windows.Forms.TextBox tb_lista_veiculos;
+        private System.Windows.Forms.Button button1;
     }
 }
 
