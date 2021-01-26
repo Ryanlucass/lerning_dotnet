7607bb0067e77d9bcc83d69abd062797d44003ad
Lucas alves <lucasryanalves@gmail.com>
2021-01-26 11:03:36 -0300

    update

--- part2/Componentes/Componentes/Form1.cs
+++ part2/Componentes/Componentes/Form1.cs
@@ -27,16 +27,19 @@ namespace Componentes
             {
                 MessageBox.Show("Digite um Veículo");
                 tb_veiculo.Focus();
-                return; //para a execução do evento
+            }
+            else if (tb_veiculo.Text == tb_lista_veiculos.Text)
+            {
+                MessageBox.Show("errado");
+            }
+            else{
+                tb_lista_veiculos.Text += tb_veiculo.Text + System.Environment.NewLine.ToString(); //pular a linha inteira 
+                                                                                                   //desse modo ele vai concatenar o texto e não substituílo
+                tb_veiculo.Clear();
+                tb_veiculo.Focus(); //onde o posicionandor de digitação irá ficar 
             }
 
-            tb_lista_veiculos.Text += tb_veiculo.Text + System.Environment.NewLine.ToString(); //pular a linha inteira 
-            //desse modo ele vai concatenar o texto e não substituílo
-            tb_veiculo.Clear();
-            tb_veiculo.Focus(); //onde o posicionandor de digitação irá ficar 
-
-  
-                      
+                       
         }
 
         private void button1_Click(object sender, EventArgs e)
