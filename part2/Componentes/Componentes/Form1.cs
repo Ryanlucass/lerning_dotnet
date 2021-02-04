cf67fedcdb340a49892af2878442db2f57aa6372
Lucas alves <lucasryanalves@gmail.com>
2021-02-04 10:27:23 -0300

    update

--- part2/Componentes/Componentes/Form1.cs
+++ part2/Componentes/Componentes/Form1.cs
@@ -1,40 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Componentes
 {
     public partial class F_Principal : Form
     {
+        //Construtor Principal
         public F_Principal()
         {
             InitializeComponent();
 
-             
+
         }
 
+        //Botão para adicionar 
         private void btn_adicionar_Click(object sender, EventArgs e)
         {
-           
+
             if (tb_veiculo.Text == string.Empty)
             {
                 MessageBox.Show("Digite um Veículo");
                 tb_veiculo.Focus();
                 return;
             }
-                 
-                tb_lista_veiculos.Text += tb_veiculo.Text + System.Environment.NewLine.ToString(); //pular a linha inteira                                                                                                  
-                tb_veiculo.Clear();
-                tb_veiculo.Focus(); //onde o posicionandor de digitação irá ficar 
-            
 
-                       
+            tb_lista_veiculos.Text += tb_veiculo.Text + System.Environment.NewLine.ToString(); //pular a linha inteira                                                                                                  
+            tb_veiculo.Clear();
+            tb_veiculo.Focus(); //onde o posicionandor de digitação irá ficar 
+
+
+
         }
         //Botão Limpar
         private void btn_clear_Click(object sender, EventArgs e)
@@ -61,13 +56,13 @@ namespace Componentes
                     break;
             }
         }
-
+        //CheckBox Menu 
         private void checkboxToolStripMenuItem_Click(object sender, EventArgs e)
         {
             F_Checkbox f_Checkbox = new F_Checkbox();
             f_Checkbox.ShowDialog();
         }
-
+        //CheckBoxList Menu
         private void checkedListBoxToolStripMenuItem_Click(object sender, EventArgs e)
         {
             F_CheckedListBox f_CheckedListBox = new F_CheckedListBox();
