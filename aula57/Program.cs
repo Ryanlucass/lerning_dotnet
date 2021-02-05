d650181cf1d1ac06898a75e72b19403c5b5dfee6
Lucas alves <lucasryanalves@gmail.com>
2021-02-05 09:38:55 -0300

    update
    Lista e Foreach

--- aula57/Program.cs
+++ aula57/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace aula57
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+        
+          List<string> carros=new List<string>();           //Nome "List"; Tipo da List; Nome da Lista.
+
+           carros.Add("Golf"); 
+           carros.Add("HB20"); 
+           carros.Add("Toro"); 
+           carros.Add("Camaro"); 
+
+           foreach(string c in carros)
+           {
+               Console.WriteLine(c);
+           }
+
         }
     }
 }
