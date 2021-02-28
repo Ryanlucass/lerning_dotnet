79fa12e9d7090b9d46924f3ea5f4eaa345fade8e
Lucas alves <lucasryanalves@gmail.com>
2021-02-28 13:37:41 -0300

    update

--- Listas_de_exercicio/04/Program.cs
+++ Listas_de_exercicio/04/Program.cs
@@ -3,15 +3,52 @@
 namespace _04
 {
 //Faça um programa que leia dois vetores de 10 elementos.
-//Crie um vetore que seja apenas os números que estão em ambos os vetores.
+//Crie um vetor que seja apenas os números que estão em ambos os vetores.
 //não deve conter números repetidos 
     class vetores
     {
 
+    // Criando os dois vetores de 10 elementos.
+
+        int[] VetorA = new int[3];
+        int[] VetorB = new int[3];
+        int[] VetorC = new int[6];
+
+
+        public void Calculo(){
+
+            for(int a=0; a<VetorA.Length; a++){
+                
+                Console.Write("diga o valor do Vetor A na posição {0}: ",a+1);
+                VetorA[a]= int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("--------------------------");
+
+            for(int b=0; b<VetorB.Length; b++){
+
+                Console.Write("diga o valor do Vetor B na posição {0}:",b+1);
+                VetorB[b]=int.Parse(Console.ReadLine());
+            }
         
+            Console.WriteLine("--------------------------");
+
+            //Chamando os valores do VetorA
+            for(int r=0; r<VetorA.Length; r++){
+
+                Console.WriteLine("A posição {0} do VetorA é: {1}",r+1,VetorA[r]);
+            }
+
+            Console.WriteLine("--------------------------");
+            //Chamando os valores do VetorB
+            
+            for(int t=0; t<VetorB.Length; t++){
+
+                Console.WriteLine("A posição {0} do VetorB é: {1}",t+1,VetorA[t]);
+            }
 
 
-
+        }
 
     }
 
@@ -20,23 +57,10 @@ namespace _04
     {
         static void Main(string[] args)
         {
-            
-            int[] n= new int[4];
+            vetores resultado = new vetores();
 
-            for(int i=0; i<n.Length; i++){
-                
-                Console.WriteLine("diga o valor da posição ");
-
-                n[i]= int.Parse(Console.ReadLine());
-            }
-
-            for(int p=0; p<n.Length; p++){
-            Console.WriteLine("A posição {0} é: {1}",p+1,n[p]);
-            }
-
-            
-            
-
+            resultado.Calculo();
+        
         }
     }
 }
