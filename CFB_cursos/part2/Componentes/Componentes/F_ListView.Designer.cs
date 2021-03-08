d931d1d3650237de11d0180a7af0270476a1fd4b
Lucas alves <lucasryanalves@gmail.com>
2021-03-08 00:22:09 -0300

    update

--- CFB_cursos/part2/Componentes/Componentes/F_ListView.Designer.cs
+++ CFB_cursos/part2/Componentes/Componentes/F_ListView.Designer.cs
@@ -30,22 +30,55 @@ namespace Componentes
         private void InitializeComponent()
         {
             this.lv_products = new System.Windows.Forms.ListView();
+            this.col_id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_produto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_qtde = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_preco = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // lv_products
             // 
+            this.lv_products.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.col_id,
+            this.col_produto,
+            this.col_qtde,
+            this.col_preco});
+            this.lv_products.FullRowSelect = true;
+            this.lv_products.GridLines = true;
             this.lv_products.HideSelection = false;
             this.lv_products.Location = new System.Drawing.Point(12, 12);
+            this.lv_products.MultiSelect = false;
             this.lv_products.Name = "lv_products";
-            this.lv_products.Size = new System.Drawing.Size(776, 161);
+            this.lv_products.Size = new System.Drawing.Size(654, 136);
             this.lv_products.TabIndex = 0;
             this.lv_products.UseCompatibleStateImageBehavior = false;
+            this.lv_products.View = System.Windows.Forms.View.Details;
+            // 
+            // col_id
+            // 
+            this.col_id.Text = "ID";
+            this.col_id.Width = 47;
+            // 
+            // col_produto
+            // 
+            this.col_produto.Text = "Produto";
+            this.col_produto.Width = 317;
+            // 
+            // col_qtde
+            // 
+            this.col_qtde.Text = "Quantidade ";
+            this.col_qtde.Width = 88;
+            // 
+            // col_preco
+            // 
+            this.col_preco.Text = "Preço";
+            this.col_preco.Width = 128;
             // 
             // F_ListView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(697, 167);
             this.Controls.Add(this.lv_products);
             this.MaximizeBox = false;
             this.Name = "F_ListView";
@@ -58,5 +91,9 @@ namespace Componentes
         #endregion
 
         private System.Windows.Forms.ListView lv_products;
+        private System.Windows.Forms.ColumnHeader col_id;
+        private System.Windows.Forms.ColumnHeader col_produto;
+        private System.Windows.Forms.ColumnHeader col_qtde;
+        private System.Windows.Forms.ColumnHeader col_preco;
     }
 }
