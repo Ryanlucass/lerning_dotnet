bcc1922e8ab17d76de14fbed47ce995adcdb96e4
Lucas alves <lucasryanalves@gmail.com>
2021-03-11 23:16:41 -0300

    update

---
+++ Listas_de_exercicio/exe03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exe03
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n1=9, n2=9;
+
+            Console.WriteLine(" A resposta é : {0}", n1+n2);
+
+
+        }
+    }
+}
