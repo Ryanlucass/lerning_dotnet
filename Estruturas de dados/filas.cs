58c0d075fe6d8fc9d226d90e984545a51bf2265a
Lucas alves <lucasryanalves@gmail.com>
2021-04-29 09:28:17 -0300

    up

---
+++ Estruturas de dados/filas.cs
@@ -0,0 +1,5 @@
+class filas{
+
+Queue<string> minhafilha
+
+}
---
+++ Estruturas de dados/filas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace filas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
